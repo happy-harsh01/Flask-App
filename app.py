37f14c10337b155981f98fe5413d7ef424c46f16
Mr.Harsh <81058137+mr-harsh-codes@users.noreply.github.com>
2022-02-01 07:48:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 from flask import Flask, render_template, url_for ,requests, redirect 
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///test.db'
+
+app.route('/', methods = ['POST', 'GET'])
+def index():
+    return "Hello World"
